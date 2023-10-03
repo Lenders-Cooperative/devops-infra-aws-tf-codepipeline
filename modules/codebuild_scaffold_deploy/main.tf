dcb0085104ced2f7fb9669ea5bb827534d127b6f
rakshay07 <109746938+rakshay07@users.noreply.github.com>
2023-10-03 18:47:02 +0530

    Update main.tf

--- modules/codebuild_scaffold_deploy/main.tf
+++ modules/codebuild_scaffold_deploy/main.tf
@@ -24,10 +24,10 @@ resource "aws_s3_bucket_public_access_block" "artifact-store_access" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_acl" "artifact-store-acl" {
-  bucket = aws_s3_bucket.artifact-store.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket_acl" "artifact-store-acl" {
+#  bucket = aws_s3_bucket.artifact-store.id
+#  acl    = "private"
+#}
 
 resource "aws_s3_bucket_versioning" "artifact-store-versioning" {
   bucket = aws_s3_bucket.artifact-store.id
